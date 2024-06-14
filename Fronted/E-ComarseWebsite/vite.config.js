21a036798e53df198ec931da42e5f5ebc2f14fd5
Bharat kushwaha <160047593+bharat711kushwaha@users.noreply.github.com>
2024-06-14 17:39:51 +0530

    Update vite.config.js

--- Fronted/E-ComarseWebsite/vite.config.js
+++ Fronted/E-ComarseWebsite/vite.config.js
@@ -4,20 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+  build: {
+    outDir: 'dist',
+  },
+});
 
-// export default defineConfig({
-//     plugins: [react()],
-//     server: {
-//         proxy: {
-//             '/api': {
-//                 target: 'http://localhost:3000',
-//                 changeOrigin: true,
-//                 secure: false,
-//             },
-//         },
-//         port: 5171, // Specify your development server port
-//     },
-// });
+})
