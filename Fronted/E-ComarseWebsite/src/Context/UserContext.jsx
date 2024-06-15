eb269fc1ac94a3f8c37774ca84504e97a668d3c3
Bharat kushwaha <160047593+bharat711kushwaha@users.noreply.github.com>
2024-06-15 09:45:33 +0530

    Update UserContext.jsx

--- Fronted/E-ComarseWebsite/src/Context/UserContext.jsx
+++ Fronted/E-ComarseWebsite/src/Context/UserContext.jsx
@@ -1,45 +1,45 @@
 // src/context/UserContext.js
 
-import React, { createContext, useReducer, useContext, useEffect } from 'react';
-import axios from 'axios';
-import userReducer from '../Reducer/userReducer';
+// import React, { createContext, useReducer, useContext, useEffect } from 'react';
+// import axios from 'axios';
+// import userReducer from '../Reducer/userReducer';
 
-const UserContext = createContext();
+// const UserContext = createContext();
 
-const initialState = {
-  user: null,
-};
+// const initialState = {
+//   user: null,
+// };
 
-export const UserProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(userReducer, initialState);
+// export const UserProvider = ({ children }) => {
+//   const [state, dispatch] = useReducer(userReducer, initialState);
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const token = localStorage.getItem('token');
-        if (token) {
-          const response = await axios.get('/api/users', {
-            headers: {
-              'x-auth-token': token,
-            },
-          });
-          dispatch({ type: 'SET_USER', payload: response.data });
-        }
-      } catch (error) {
-        console.error('Error fetching user data:', error);
-      }
-    };
+//   useEffect(() => {
+//     const fetchUser = async () => {
+//       try {
+//         const token = localStorage.getItem('token');
+//         if (token) {
+//           const response = await axios.get('/api/users', {
+//             headers: {
+//               'x-auth-token': token,
+//             },
+//           });
+//           dispatch({ type: 'SET_USER', payload: response.data });
+//         }
+//       } catch (error) {
+//         console.error('Error fetching user data:', error);
+//       }
+//     };
 
-    fetchUser();
-  }, []);
+//     fetchUser();
+//   }, []);
 
-  return (
-    <UserContext.Provider value={{ state, dispatch }}>
-      {children}
-    </UserContext.Provider>
-  );
-};
+//   return (
+//     <UserContext.Provider value={{ state, dispatch }}>
+//       {children}
+//     </UserContext.Provider>
+//   );
+// };
 
-export const useUserContext = () => {
-  return useContext(UserContext);
-};
+// export const useUserContext = () => {
+//   return useContext(UserContext);
+// };
