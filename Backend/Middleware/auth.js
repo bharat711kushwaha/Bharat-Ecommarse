c63e9496bb42bfc8d69d1dd163eebc1e2d5f563c
Bharat kushwaha <160047593+bharat711kushwaha@users.noreply.github.com>
2024-06-15 21:06:05 +0530

    Update auth.js

--- Backend/Middleware/auth.js
+++ Backend/Middleware/auth.js
@@ -8,8 +8,9 @@ const auth = (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, 'your_jwt_secret');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.user;
+
     next();
   } catch (err) {
     res.status(401).json({ msg: 'Token is not valid' });
